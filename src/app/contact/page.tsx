7921f79b4c758874e2f2fdf0b2341fbad298a121
Gagandeep Singh <deepgagan9956@gmail.com>
2023-11-13 00:01:42 +0530

    solve contact page error

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -14,7 +14,7 @@ const Contact = () => {
           <ContactForm />
         </section>
       </div>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m25!1m12!1m3!1d3556.6970172445954!2d75.78314722590382!3d26.944818076628007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m10!3e6!4m3!3m2!1d26.9448968!2d75.7857255!4m4!2s26.944778%2C75.785738!3m2!1d26.944778!2d75.785738!5e0!3m2!1sen!2sin!4v1699769573562!5m2!1sen!2sin" width={100} height={450} style={{ border: 0 }} allowFullScreen="" className={styles.mapping} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m25!1m12!1m3!1d3556.6970172445954!2d75.78314722590382!3d26.944818076628007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m10!3e6!4m3!3m2!1d26.9448968!2d75.7857255!4m4!2s26.944778%2C75.785738!3m2!1d26.944778!2d75.785738!5e0!3m2!1sen!2sin!4v1699769573562!5m2!1sen!2sin" width={100} height={450} style={{ border: 0 }} allowFullScreen={true} className={styles.mapping} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
     </>
   )
 }
